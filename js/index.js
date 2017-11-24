80e6bc195170416d3e70c0d6f65b3f7f60c4b2e2
Jack Loughran <loughranjack28@gmail.com>
2017-11-24 17:40:15 -0500

    integrated aplayer

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var ap = new APlayer({
+  music: {                                                           // Required, music info, see: ###With playlist
+            title: 'Preparation',                                          // Required, music title
+            author: 'Hans Zimmer/Richard Harvey',                          // Required, music author
+            url: 'http://7xifn9.com1.z0.glb.clouddn.com/Preparation.mp3',  // Required, music url
+            pic: 'http://7xifn9.com1.z0.glb.clouddn.com/Preparation.jpg',  // Optional, music picture
+        }
+});
+
