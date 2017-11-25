29fbebeb4b7a9bbc218650a28cffbb521f87d823
Jack Loughran <loughranjack28@gmail.com>
2017-11-24 21:25:43 -0500

    songs populate somewhat incorrectly

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
+var ap = new APlayer({
+  music: {}
+});
+
 var xhr = new XMLHttpRequest({mozSystem: true});
 xhr.open("GET", "http://localhost:48001/api/list", true)
 xhr.onload = function (e) {
   if (xhr.readyState === 4) {
     if (xhr.status === 200) {
       var song = JSON.parse(xhr.responseText);
-      makePlayer(song);
+      listArtists(song);
     } else {
       console.error(xhr.statusText);
     }
@@ -15,13 +19,81 @@ xhr.onerror = function (e) {
 };
 xhr.send(null);
 
-function makePlayer(songs) {
-  console.log("Song: " + songs[0].loc)
-  var ap = new APlayer({
-    music: {                                                           // Required, music info, see: ###With playlist
-              title: songs[0].title,                                          // Required, music title
-              author: songs[0].artist,                          // Required, music author
-              url: songs[0].loc,  // Required, music url
-          }
+function listArtists(songs) {
+
+  var artistMap = parseSongs(songs);
+
+  var app = document.getElementById('app');
+
+  artistMap.forEach(function (value, key, map) {
+    var li = document.createElement('li');
+    li.innerHTML = key;
+
+    var clickFunction = function () {
+      populateAlbums(li, value, key)
+      li.removeEventListener('click', clickFunction)
+    }
+    li.addEventListener('click', clickFunction);
+
+    app.appendChild(li);
   });
+}
+
+function parseSongs(songs) {
+  var artistMap = new Map();
+  for (var i = 0; i < songs.length; i++) {
+    if (artistMap.get(songs[i].artist) === undefined) {
+      var titleMap = new Map([[songs[i].title, songs[i].loc]]);
+      var albumMap = new Map([[songs[i].album, titleMap]]);
+      artistMap.set(songs[i].artist, albumMap);
+    } else {
+      var albumMap = artistMap.get(songs[i].artist);
+      if (albumMap.get(songs[i].album) === undefined) {
+        var titleMap = new Map([[songs[i].title, songs[i].loc]]);
+        albumMap.set(songs[i].album, titleMap);
+      } else {
+        var titleMap = albumMap.get(songs[i].album);
+        titleMap.set(songs[i].title, songs[i].loc);
+        albumMap.set(songs[i].album, titleMap);
+      }
+    }
+  }
+
+  return artistMap;
+}
+
+function populateAlbums(element, albumMap, artist) {
+  var ul = document.createElement('ul');
+  albumMap.forEach(function (value, key) {
+    var li = document.createElement('li');
+
+    li.innerHTML = key
+
+    var clickFunction = function () {
+      populatePlayer(value, artist)
+      li.removeEventListener('click', clickFunction)
+    }
+    li.addEventListener('click', clickFunction)
+    ul.appendChild(li)
+  });
+
+  element.appendChild(ul)
+}
+
+function populatePlayer(titleMap, artist) {
+  console.log(artist)
+  var music = []
+  titleMap.forEach(function (value, key) {
+    music.push({
+      title: key,
+      artist: '',
+      url: value
+    })
+  })
+
+  for (var i = 0; i < music.length; i++) {
+    music[i].artist = artist
+  }
+
+  ap.addMusic(music)
 }
